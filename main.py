f93a8dc0f053c4f4eccfed2411bbc544430e5ce6
Orel Mizrahi <76569702+orelmizrahii@users.noreply.github.com>
2022-03-14 05:51:53 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+sum1 = float(input('PRICE OF LIMITIED : '))
+fee1 = 0.7
+fee2 = 0.7
+rate1 = 5.8
+output = float(sum1) * float(fee1) * float(fee2) * float(rate1)
+
+print("PRICE  |MINUS|MINUS|RATE |PRICE  |")
+print("LIMITED|30%  |30%  |OF   |IN     |")
+print("FORSALE|FEE  |FEE  |ROBUX|USD    |")
+print("PRICE  |MINUS|MINUS|RATE |PRICE  |")
+print("IN R$  |     |     |     |$$$    |")
+print("MARKET |")
+
+print(output)
+print(" IF GREATER PROFIT/LOSS")
